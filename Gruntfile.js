c9b4f3b0ad6d6f959c0a69ef2d16cb0963945677
Ionut Colceriu <contact@ghinda.net>
2014-05-23 15:12:48 +0300

    New editor configs and gitignore.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,8 @@
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
 var mountFolder = function (connect, dir) {
-	return connect.static(require('path').resolve(dir));
+  return connect.static(require('path').resolve(dir));
 };
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
 
 module.exports = function (grunt) {
 	// load all grunt tasks
